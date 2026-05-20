616a449bb7277f7a36607a1eafba544fd7b93d19
Krish Kumar <Krishsinghania6115@gmail.com>
2026-05-20 14:53:36 +0530

    Update API base URL to use VITE environment variable

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: `${import.meta.env.SERVER_API_URL}/api`,
+  baseURL: `${import.meta.env.VITE_SERVER_API_URL}/api`,
   timeout: 10000,
   headers: {
     "Content-Type": "application/json",
